c8af88f667b6ab7ebaf58913008984f151d3eafb
Alan Egan <alan.mac.aodhagain@gmail.com>
2018-06-29 13:43:14 +0100

    Adds simple sample app, App can login, logout, alias, group, and track an event.

---
+++ ConnectSample/ConnectSample-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  ConnectSample-Bridging-Header.h
+//  Outbound
+//
+//  Created by Alan Egan on 19/06/2018.
+//  Copyright © 2018 Outbound.io. All rights reserved.
+//
+
+#import "Outbound.h"
